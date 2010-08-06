82a3b3c641168b5e366590d0d5b8bfc3f87fb434
Alexander Samoilov <snipka@gmail.com>
2010-08-06 15:32:45 -0400

    facilities added for setting up Dev environment

--- dev.py
+++ dev.py
@@ -5,6 +5,6 @@ incoming_file_dir = "/homes/borgii/snipka/FTP"
 def generate_dummy_fits():
     #rawdata_re_pattern = r"^p2030.*b[0-7]s[01]g?.*\.fits$"
     for i in range(5):
-        f = open(incoming_file_dir+"/"+"p2030_"+i+"_b0s0g_"+i+".fits",'w')
+        f = open(incoming_file_dir+"/"+"p2030_"+str(i)+"_b0s0g_"+str(i)+".fits",'w')
         f.close
     return True
