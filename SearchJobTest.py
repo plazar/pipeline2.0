1e8eff878b55ab0380ebbb183e893e0a681cb781
Alexander Samoilov <snipka@gmail.com>
2011-01-11 13:47:09 -0500

    added delete funtion for the Job object to remove from the QSUB

--- SearchJobTest.py
+++ SearchJobTest.py
@@ -1,9 +1,13 @@
 import job
 
 datafile = ['/data/alfa/test_pipeline/p2030_54553_43341_0091_G50.62-00.03.S_2.w4bit.fits']
-sj = job.PulsarSearchJob(datafile)
-sj.submit()
-sj.get_qsub_status()
-print "status: %s " % str(sj.status)
 
-print sj.delete()
+print "Creating the job..."
+sj = job.PulsarSearchJob(datafile)
+print "Submitting the job to QSUB..."
+sj.submit()
+print "Getting QSUB status of the job..."
+sj.get_qsub_status()
+print "Status: %s " % str(sj.status)
+print "Removing the job from QSUB..."
+print "Removed?: %s" % sj.delete()
