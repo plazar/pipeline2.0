a92f80c3e5c7c947cadf6fd46f3e1fe555896406
Patrick Lazarus <plazar@physics.mcgill.ca>
2011-02-28 16:59:39 -0500

    Modified 'show_pipeline_stats.py' to use new statuses in job-tracker DB and to not use job_uploads table.

--- show_pipeline_stats.py
+++ show_pipeline_stats.py
@@ -7,29 +7,34 @@ import jobtracker
 import config.download
 
 create_times = jobtracker.query("SELECT JULIANDAY(created_at) FROM jobs")
-upload_times = jobtracker.query("SELECT JULIANDAY(job_uploads.updated_at) " \
-                                    "FROM jobs, job_uploads " \
-                                    "WHERE jobs.id = job_uploads.job_id AND " \
-                                        "job_uploads.status='uploaded'")
+
+upload_times = jobtracker.query("SELECT JULIANDAY(updated_at) FROM jobs " \
+                                    "WHERE status='uploaded'")
+
 fail_times = jobtracker.query("SELECT JULIANDAY(updated_at) FROM jobs " \
                                     "WHERE status='terminal_failure'")
 
 bytes_downloaded = jobtracker.query("SELECT size, JULIANDAY(updated_at) " \
                                     "FROM downloads " \
                                     "WHERE status='downloaded'")
-bytes_deleted_pass = jobtracker.query("SELECT -SUM(downloads.size), MAX(JULIANDAY(job_uploads.updated_at)) " \
-                                    "FROM job_uploads, downloads, job_files " \
-                                    "WHERE downloads.id=job_files.file_id AND job_uploads.job_id=job_files.job_id AND job_uploads.status='uploaded'" \
-                                    "GROUP BY job_files.job_id")
-bytes_deleted_fail = jobtracker.query("SELECT -SUM(downloads.size), MAX(JULIANDAY(jobs.updated_at)) " \
-                                    "FROM jobs, downloads, job_files " \
-                                    "WHERE downloads.id=job_files.file_id AND jobs.id=job_files.job_id AND jobs.status='terminal_failure'" \
-                                    "GROUP BY job_files.job_id")
+bytes_deleted_pass = jobtracker.query("SELECT -SUM(downloads.size), " \
+                                            "MAX(JULIANDAY(jobs.updated_at)) " \
+                                      "FROM jobs, downloads, job_files " \
+                                      "WHERE downloads.id=job_files.file_id " \
+                                            "AND jobs.id=job_files.job_id " \
+                                            "AND jobs.status='uploaded'" \
+                                      "GROUP BY jobs.id")
+bytes_deleted_fail = jobtracker.query("SELECT -SUM(downloads.size), " \
+                                            "MAX(JULIANDAY(jobs.updated_at)) " \
+                                      "FROM jobs, downloads, job_files " \
+                                      "WHERE downloads.id=job_files.file_id " \
+                                            "AND jobs.id=job_files.job_id " \
+                                            "AND jobs.status='terminal_failure'" \
+                                      "GROUP BY jobs.id")
 
-
-create_times = np.asarray([row[0] for row in create_times])
-upload_times = np.asarray([row[0] for row in upload_times])
-fail_times = np.asarray([row[0] for row in fail_times])
+create_times = np.asarray(sorted([row[0] for row in create_times]))
+upload_times = np.asarray(sorted([row[0] for row in upload_times]))
+fail_times = np.asarray(sorted([row[0] for row in fail_times]))
 
 bytes_times = bytes_downloaded + bytes_deleted_pass + bytes_deleted_fail
 bytes = np.asarray([row[0] for row in bytes_times])
@@ -44,7 +49,7 @@ print "Num jobs failed:", len(fail_times)
 
 day0 = np.concatenate((create_times, times)).min()
 
-plt.figure()
+fig = plt.figure()
 jobax = plt.subplot(2,1,1)
 plt.plot(create_times-day0, np.arange(1, len(create_times)+1), 'k-', lw=2)
 plt.plot(upload_times-day0, np.arange(1, len(upload_times)+1), 'g-')
@@ -55,4 +60,8 @@ plt.plot(times-day0, bytes/1024.0**3, 'k-')
 plt.axhline(config.download.space_to_use/1024.0**3, c='k', ls=':')
 plt.xlabel("Days since pipeline started")
 plt.ylabel("Pipeline disk usage (raw data, GB)")
+
+fig.canvas.mpl_connect("key_press_event", \
+            lambda e: (e.key in ('q', 'Q') and plt.close(fig)))
+
 plt.show()
