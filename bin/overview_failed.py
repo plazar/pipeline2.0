ad539eb5281bf57a4fcc8a6401d03159aea18b9e
Patrick Lazarus <plazar@physics.mcgill.ca>
2011-03-07 15:45:58 -0500

    Update output of 'overview_failed.py' utility.
    Also, added command line option for full output.

--- bin/overview_failed.py
+++ bin/overview_failed.py
@@ -1,16 +1,37 @@
+import optparse
 import jobtracker
-"""
-This scripts displays details about jobs that failed and failed permanently.
-"""
-failed_jobs = jobtracker.query("SELECT * FROM jobs WHERE status='terminal_failure' AND status='failed'")
-output = ''
-for job_row in failed_jobs:
-    failed_submits = jobtracker.query("SELECT * FROM job_submits WHERE job_id=%u" % \
-                                        job_row['id'])
-    output += str(job_row['id'])
-    for submit in failed_submits:
-        output += '\t'+ str(submit['details']) +'\t'+ str(submit['output_dir']) + \
-                    '\t'+ str(submit['updated_at'])+'\n'
-    output += '\n'
 
-print output
+"""
+Displays details about jobs that are terminally failed.
+"""
+def main():
+    failed_jobs = jobtracker.query("SELECT id, updated_at FROM jobs " \
+                                   "WHERE status='terminal_failure'" \
+                                   "ORDER BY updated_at ASC")
+    for job in failed_jobs:
+        last_submit = jobtracker.query("SELECT id, job_id, status, details, updated_at " \
+                                       "FROM job_submits " \
+                                       "WHERE job_id=%d " \
+                                       "ORDER BY updated_at DESC" % job['id'], \
+                                       fetchone=True)
+        headerline = "========== Job ID: %d, last job submission ID: %d ==========" % \
+            (last_submit['job_id'], last_submit['id'])
+        print headerline
+        print "Last job submission status: %s (%s)" % \
+            (last_submit['status'], last_submit['updated_at'])
+        if options.full:
+            print last_submit['details']
+        print "-"*len(headerline)
+        print ""
+
+if __name__ == '__main__':
+    parser = optparse.OptionParser(usage="%prog [OPTIONS]", \
+                                   description="Show an overview of all " \
+                                        "terminally failed jobs.")
+    parser.add_option('-f', '--full', dest='full', action='store_true', \
+                        help="Show full details for each entry displayed. " \
+                                "(Default: Omit details).", \
+                        default=False)
+    options, args = parser.parse_args()
+    main()
+    
