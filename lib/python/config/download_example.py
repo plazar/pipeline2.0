65573cae8e42226cf1e73507358b3316f57f3593
Patrick Lazarus <plazar@physics.mcgill.ca>
2011-03-09 16:49:52 -0500

    Removed passwords from 'download_example.py' config file.

--- lib/python/config/download_example.py
+++ lib/python/config/download_example.py
@@ -4,12 +4,12 @@ import config.basic
 # Downloader Configuration
 ################################################################
 api_service_url = "http://arecibo.tc.cornell.edu/palfadataapi/dataflow.asmx?WSDL"
-api_username = "mcgill"
-api_password = "palfa@Mc61!!"
+api_username = "username"
+api_password = "password"
 ftp_host = "arecibo.tc.cornell.edu"
 ftp_port = 31001
-ftp_username = "palfadata"
-ftp_password = "NAIC305m"
+ftp_username = "username"
+ftp_password = "password"
 temp = "/data/alfa/test_pipeline_clean/"
 space_to_use = 228748364800
 numdownloads = 2
