4701a5e1e507f37b1bd971530c44bc5afc2aee49
Patrick Lazarus <plazar@physics.mcgill.ca>
2011-03-09 14:23:07 -0500

    Removed comment about non-existing table in 'create_database.py'.

--- bin/create_database.py
+++ bin/create_database.py
@@ -60,15 +60,6 @@ creates.append("CREATE TABLE requests ( " \
                     "status TEXT, " \
                     "updated_at TEXT)")
 
-########## No Longer in use ####################
-#creates.append("CREATE TABLE job_uploads ( " \
-#                    "created_at TEXT, " \
-#                    "details TEXT, " \
-#                    "id INTEGER PRIMARY KEY, " \
-#                    "job_id INTEGER, " \
-#                    "status TEXT, " \
-#                    "updated_at TEXT)")
-
 if not os.path.exists(config.background.jobtracker_db):
     print "Database file %s doesn't exists, creating a clean database." % \
                     config.background.jobtracker_db
