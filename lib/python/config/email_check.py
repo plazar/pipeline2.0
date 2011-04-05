e0b456177f37bef52a327ee351b2a7a471dc4e80
Scott Ransom <sransom@nrao.edu>
2011-04-05 08:51:14 +0800

    Allow None for SMTP host

--- lib/python/config/email_check.py
+++ lib/python/config/email_check.py
@@ -2,7 +2,7 @@ import config_types
 
 email = config_types.ConfigList('email')
 email.add_config('enabled', config_types.BoolConfig())
-email.add_config('smtp_host', config_types.StrConfig())
+email.add_config('smtp_host', config_types.StrOrNoneConfig())
 email.add_config('smtp_username', config_types.StrConfig())
 email.add_config('smtp_password', config_types.StrConfig())
 email.add_config('recipient', config_types.StrConfig())
