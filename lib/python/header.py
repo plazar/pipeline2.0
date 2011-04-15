13f17333f51cbe78f11bc1a0685edb4249b431c6
Patrick Lazarus <plazar@physics.mcgill.ca>
2011-04-15 04:57:30 -0400

    Modified 'header.py' to have a function to compare on-disk results with database.

--- lib/python/header.py
+++ lib/python/header.py
@@ -16,6 +16,7 @@ import optparse
 
 import upload
 import datafile
+import database
 
 # Raise warnings produced by invalid coord strings as exceptions
 warnings.filterwarnings("error", message="Input is not a valid sexigesimal string: .*")
@@ -73,7 +74,110 @@ class Header(upload.Uploadable):
             "@ra_deg=%.8f, " % self.ra_deg + \
             "@dec_deg=%.8f" % self.dec_deg
         return sprocstr
-    
+
+    def compare_with_db(self):
+        """Grab corresponding header from DB and compare values.
+            Return True if all values match. Return False otherwise.
+
+            Inputs:
+                None
+            Outputs:
+                match: Boolean. True if all values match, False otherwise.
+        """
+        db = database.Database('common-copy')
+        db.execute("SELECT obs.obs_name, " \
+                          "h.beam_id, " \
+                          "h.original_wapp_file, " \
+                          "h.sample_time, " \
+                          "h.observation_time, " \
+                          "h.timestamp_mjd, " \
+                          "h.num_samples_per_record, " \
+                          "h.center_freq, " \
+                          "h.channel_bandwidth, " \
+                          "h.num_channels_per_record, " \
+                          "h.num_ifs, " \
+                          "h.orig_right_ascension, " \
+                          "h.orig_declination, " \
+                          "h.orig_galactic_longitude, " \
+                          "h.orig_galactic_latitude, " \
+                          "h.source_name, " \
+                          "h.sum_id, " \
+                          "h.orig_start_az, " \
+                          "h.orig_start_za, " \
+                          "h.start_ast, " \
+                          "h.start_lst, " \
+                          "h.project_id, " \
+                          "h.observers, " \
+                          "h.file_size, " \
+                          "h.data_size, " \
+                          "h.num_samples, " \
+                          "h.orig_ra_deg, " \
+                          "h.orig_dec_deg, " \
+                          "h.right_ascension, " \
+                          "h.declination, " \
+                          "h.galactic_longitude, " \
+                          "h.galactic_latitude, " \
+                          "h.ra_deg, " \
+                          "h.dec_deg, " \
+                          "h.obsType " \
+                   "FROM headers AS h " \
+                   "LEFT JOIN observations AS obs ON obs.obs_id=h.obs_id " \
+                   "WHERE obs.obs_name='%s' AND h.beam_id=%d " % \
+                        (self.obs_name, self.beam_id))
+        rows = db.cursor.fetchall()
+        if not rows:
+            # No matching entry in common DB
+            raise ValueError("No matching entry in common DB!\n" \
+                                "(obs_name: %s, beam_id: %d)" % \
+                                (self.obs_name, self.beam_id))
+        elif len(rows) > 1:
+            # Too many matching entries!
+            raise ValueError("Too many matching entries in common DB!\n" \
+                                "(obs_name: %s, beam_id: %d)" % \
+                                (self.obs_name, self.beam_id))
+        else:
+            desc = [d[0] for d in db.cursor.description]
+            r = dict(zip(desc, rows[0]))
+            matches = [('%s' % r['obs_name'].lower() == '%s' % self.obs_name.lower()),  \
+                     ('%d' % r['beam_id'] == '%d' % self.beam_id),  \
+                     ('%s' % r['original_wapp_file'].lower() == '%s' % self.original_file.lower()),  \
+                     ('%f' % r['sample_time'] == '%f' % self.sample_time),  \
+                     ('%f' % r['observation_time'] == '%f' % self.observation_time),  \
+                     ('%.15f' % r['timestamp_mjd'] == '%.15f' % self.timestamp_mjd),  \
+                     ('%d' % r['num_samples_per_record'] == '%d' % self.num_samples_per_record),  \
+                     ('%f' % r['center_freq'] == '%f' % self.center_freq),  \
+                     ('%f' % r['channel_bandwidth'] == '%f' % self.channel_bandwidth),  \
+                     ('%d' % r['num_channels_per_record'] == '%d' % self.num_channels_per_record),  \
+                     ('%d' % r['num_ifs'] == '%d' % self.num_ifs),  \
+                     ('%.4f' % r['orig_right_ascension'] == '%.4f' % self.orig_right_ascension),  \
+                     ('%.4f' % r['orig_declination'] == '%.4f' % self.orig_declination),  \
+                     ('%.8f' % r['orig_galactic_longitude'] == '%.8f' % self.orig_galactic_longitude),  \
+                     ('%.8f' % r['orig_galactic_latitude'] == '%.8f' % self.orig_galactic_latitude),  \
+                     ('%s' % r['source_name'].lower() == '%s' % self.source_name.lower()),  \
+                     ('%d' % r['sum_id'] == '%d' % self.sum_id),  \
+                     ('%.4f' % r['orig_start_az'] == '%.4f' % self.orig_start_az),  \
+                     ('%.4f' % r['orig_start_za'] == '%.4f' % self.orig_start_za),  \
+                     ('%.8f' % r['start_ast'] == '%.8f' % self.start_ast),  \
+                     ('%.8f' % r['start_lst'] == '%.8f' % self.start_lst),  \
+                     ('%s' % r['project_id'].lower() == '%s' % self.project_id.lower()),  \
+                     ('%s' % r['observers'].lower() == '%s' % self.observers.lower()),  \
+                     ('%d' % r['file_size'] == '%d' % self.file_size),  \
+                     ('%d' % r['data_size'] == '%d' % self.data_size),  \
+                     ('%d' % r['num_samples'] == '%d' % self.num_samples),  \
+                     ('%.8f' % r['orig_ra_deg'] == '%.8f' % self.orig_ra_deg),  \
+                     ('%.8f' % r['orig_dec_deg'] == '%.8f' % self.orig_dec_deg),  \
+                     ('%.4f' % r['right_ascension'] == '%.4f' % self.right_ascension),  \
+                     ('%.4f' % r['declination'] == '%.4f' % self.declination),  \
+                     ('%.8f' % r['galactic_longitude'] == '%.8f' % self.galactic_longitude),  \
+                     ('%.8f' % r['galactic_latitude'] == '%.8f' % self.galactic_latitude),  \
+                     ('%.8f' % r['ra_deg'] == '%.8f' % self.ra_deg),  \
+                     ('%.8f' % r['dec_deg'] == '%.8f' % self.dec_deg),  \
+                     ('%s' % r['obsType'].lower() == '%s' % self.obstype.lower())]
+
+            # Match is True if _all_ matches are True
+            match = all(matches)
+        return match
+
 
 class HeaderError(Exception):
     """Error to throw when a header-specific problem is encountered.
@@ -134,7 +238,13 @@ def upload_header(fns, beamnum=None, verbose=False, dry_run=False, \
 
 def main():
     try:
-        upload_header(args, options.beamnum, options.verbose, options.dry_run)
+        # upload_header(args, options.beamnum, options.verbose, options.dry_run)
+
+        ###### Test comparison with DB
+        header = Header(args)
+        print header.compare_with_db()
+        ######
+
     except upload.UploadError, e:
         traceback.print_exception(*sys.exc_info())
         sys.stderr.write("\nOriginal exception thrown:\n")
