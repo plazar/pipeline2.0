45835a403bb17bff7d6ffea12f0c17b3543cf4b5
Patrick Lazarus <plazar@physics.mcgill.ca>
2011-04-22 22:26:19 -0400

    Updated 'jobpooler_example.py' to be consistent with modifications to PBS queue manager interface.

--- lib/python/config/jobpooler_example.py
+++ lib/python/config/jobpooler_example.py
@@ -10,8 +10,9 @@ base_results_directory = "/data/data7/PALFA/test_new_pipeline_clean/"
 max_jobs_running = 50 # maximum number of running jobs
 max_jobs_queued = 1  # Can be kept small so that you don't hog the queue (>=1)
 max_attempts = 2 # Maximum number of times a job is attempted due to errors
+# Arguments to pbs.PBSManager are (node name, node property, max # cores to use)
 queue_manager = queue_managers.pbs.PBSManager("%s_batchjob" % config.basic.survey, \
-                                    "nodes=P248:ppn=1")
+                                    "node-property", 8)
 
 import jobpooler_check
 jobpooler_check.jobpooler.populate_configs(locals())
