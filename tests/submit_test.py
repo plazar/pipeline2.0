1a82a42b56c0666988aa03ba113a1df1b3abd747
Patrick Lazarus <plazar@physics.mcgill.ca>
2011-05-07 16:31:10 -0400

    Add 'test_job.py', a job to test the set-up on worker nodes, and 'submit_job.py' which runs the job and checks the output for errors.

---
+++ tests/submit_test.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+"""
+Submit 'test_job.py' to the queue and wait for it to terminate.
+Then check for errors.
+"""
+import sys
+import time
+import os.path
+
+import config.basic
+import config.jobpooler
+
+# Submit 'test_job.py' to the queue.
+print "Submitting job to queue"
+queue_id = config.jobpooler.queue_manager.submit(datafiles=['testfn1', 'testfn2'], \
+                                        outdir='/fake/out/dir/', \
+                                        script=os.path.join(config.basic.pipelinedir, \
+                                                            'tests', 'test_job.py'))
+count = 1
+sys.stdout.write("\rWaiting for job to terminate     ")
+sys.stdout.flush()
+while config.jobpooler.queue_manager.is_running(queue_id):
+    sys.stdout.write("\rWaiting for job to terminate" + \
+                            "."*(count%6) + " "*(5-count%6))
+    sys.stdout.flush()
+    count = (count+1)%6
+    time.sleep(1)
+
+print "\nJob is done. Checking for errors."
+if config.jobpooler.queue_manager.had_errors(queue_id):
+    print "="*60
+    print "Error report from 'test_job.py:"
+    print config.jobpooler.queue_manager.get_errors(queue_id)
+    print "="*60
+    sys.exit(1)
+else:
+    print "No errors from 'test_job.py'..."
+
---
+++ tests/test_job.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python
+"""
+A test job to be submitted to the queue.
+
+Patrick Lazarus, May 6, 2011
+"""
+import os
+import os.path
+import sys
+import subprocess
+import socket
+
+import config.jobpooler
+import config.processing
+import config.searching
+
+def system_call(cmd):
+    pipe = subprocess.Popen(cmd, shell=True, \
+                                stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = pipe.communicate()
+    retcode = pipe.returncode
+    if retcode < 0:
+        raise SystemCallError("System call (%s) terminated by signal (%s)!" % \
+                                (cmd, -retcode))
+    elif retcode > 0:
+        raise SystemCallError("System call (%s) failed with status (%s)!" % \
+                                (cmd, retcode))
+    else:
+        # Exit code is 0, which is "Success". Do nothing.
+        pass
+    return out, err
+
+
+class SystemCallError(Exception):
+    """An exception to throw when a system call returns 
+        with a non-zero exit code.
+    """
+    pass
+
+
+def check_env():
+    """Check if enviroment variables are set.
+
+        Return an error message, which is an empty string
+        if no errors were encountered.
+    """
+    to_test = ["DATAFILES", "OUTDIR"]
+    errors = ""
+    for var in to_test:
+        print "Testing %s" % var
+        if os.getenv(var) is None:
+            errors += "%s environment variable is not set in test job!\n" % var
+    return errors
+
+
+def check_software():
+    """Check if software needed by pipeline is installed.
+
+        Return an error message, which is an empty string
+        if no errors were encountered.
+    """
+    to_test = ["rfifind", "prepsubband", "single_pulse_search.py", \
+                "realfft", "zapbirds", "rednoise", "accelsearch", \
+                "prepfold", "convert", "gzip", "tempo"]
+    errors = ""
+    for prog in to_test:
+        print "Testing %s" % prog
+        try:
+            out, err = system_call("which %s" % prog)
+        except SystemCallError:
+            errors += "The program '%s' cannot be found.\n" % prog
+    return errors
+
+
+def check_dirs():
+    to_test = [config.processing.zaplistdir, \
+                config.processing.base_working_directory, \
+                config.searching.base_tmp_dir, \
+                config.jobpooler.base_results_directory]
+    errors = ""
+    for dir in to_test:
+        print "Testing %s" % dir
+        if not (os.path.isdir(dir) and os.access(dir, os.R_OF | os.W_OK)):
+            errors += "The directory '%s' either doesn't exist, " \
+                        "or doesn't have read/write permission!\n" % dir
+    return errors
+
+
+def check_modules():
+    to_test = ["numpy", "pyfits", "psr_utils", "presto", "sifting"]
+    errors = ""
+    for m in to_test:
+        print "Testing %s" % m
+        try:
+            __import__(m)
+        except Exception, e:
+            errors += "Error importing %s - %s: %s\n" % (m, type(e), str(e))
+    return errors
+
+
+def main():
+    error_msg = ""
+    error_msg += check_env()
+    error_msg += check_software()
+    error_msg += check_dirs()
+    error_msg += check_modules()
+
+    if error_msg:
+        sys.stderr.write("Ran on %s\n\n" % socket.gethostname())
+        sys.stderr.write("Errors:\n")
+        sys.stderr.write(error_msg)
+
+
+if __name__=='__main__':
+    main()
