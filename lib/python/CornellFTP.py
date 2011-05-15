8b982322bd933b47181714f8e33244b4254f8590
Patrick Lazarus <plazar@physics.mcgill.ca>
2011-05-15 09:25:54 -0400

    Updated 'CornellFTP.py' to include a method to upload to Cornell's FTP server.
    Also, updated 'cornellftp_test.py' to take advantage of this feature.

--- lib/python/CornellFTP.py
+++ lib/python/CornellFTP.py
@@ -12,104 +12,91 @@ cout = OutStream.OutStream("CornellFTP Module", \
                 os.path.join(config.basic.log_dir, "downloader.log"), \
                 config.background.screen_output)
 
-class CornellFTP():
+class CornellFTP(M2Crypto.ftpslib.FTP_TLS):
+    def __init__(self, host=config.download.ftp_host, \
+                        port=config.download.ftp_port, \
+                        username=config.download.ftp_username, \
+                        password=config.download.ftp_password, \
+                        *args, **kwargs):
 
-    def __init__(self):
-        self.username = config.download.ftp_username
-        self.password = config.download.ftp_password
-        self.host =config.download.ftp_host
-        self.port =config.download.ftp_port
+        M2Crypto.ftpslib.FTP_TLS.__init__(self, *args, **kwargs)
+        try:
+            self.connect(host, port)
+            self.auth_tls()
+            self.set_pasv(1)
+            self.login(username, password)
+        except Exception, e:
+            raise CornellFTPError(str(e))
+        else:
+            cout.outs("CornellFTP - Connected and logged in")
 
-    def get_files(self,ftp_path):
-        try_counter = 0
-        login=False
-        while not login:
-            try_counter+=1
-            try:
-                myFtp = self.login(self.connect())
-                files_list = self.list_files(myFtp,ftp_path)
-                files_and_size = list()
-                login = True
-            except (CornellFTPConnectionError,CornellFTPLoginError),e:
-                print str(e)
-                if try_counter > 7:
-                    try:
-                        notification = mailer.ErrorMailer('CornellFTP login failure, retried %u times: %s' % (str(e),try_counter), subject="FTP log-in failure!")
-                        notification.send()
-                    except Exception,e:
-                        pass
+    def __del__(self):
+        self.quit()
 
-        for file in files_list:
-            files_and_size.append( (file, myFtp.size(os.path.join(ftp_path,file)) ) )
-        myFtp.close()
-        return files_and_size
+    def list_files(self, ftp_path):
+        return self.nlst(ftp_path)
 
-    def download(self,ftp_file_path):
-        try_counter = 0
-        login=False
-        while not login:
-            try_counter+=1
-            try:
-                myFtp = self.login(self.connect())
-                login = True
-            except (CornellFTPConnectionError,CornellFTPLoginError),e:
-                print str(e)
-                if try_counter > 7:
-                    try:
-                        notification = mailer.ErrorMailer('CornellFTP login failure, retried %u times: %s' % (str(e),try_counter), subject="FTP log-in failure")
-                        notification.send()
-                    except Exception,e:
-                        pass
-        localfn = os.path.join(config.download.datadir,os.path.basename(ftp_file_path))
-        self.downloading_file = open(localfn, 'wb')
-        myFtp.sendcmd("TYPE I")
+    def get_files(self, ftp_path):
+        files = self.list_files(ftp_path)
+        sizes = [self.size(os.path.join(ftp_path, fn)) for fn in files]
+        return zip(files, sizes)
+
+    def download(self, ftp_path):
+        localfn = os.path.join(config.download.datadir,os.path.basename(ftp_path))
+        f = open(localfn, 'wb')
+        
+        # Define a function to write blocks to the file
+        def write(block):
+            f.write(block)
+            f.flush()
+            os.fsync(f)
+        
+        self.sendcmd("TYPE I")
         cout.outs("CornellFTP - Starting Download of: %s" % \
-                        os.path.split(ftp_file_path)[-1])
-        myFtp.retrbinary("RETR "+ftp_file_path, self.write)
-        self.downloading_file.close()
-        myFtp.close()
+                        os.path.split(ftp_path)[-1])
+        self.retrbinary("RETR "+ftp_path, write)
+        f.close()
         cout.outs("CornellFTP - Finished download of: %s" % \
-                        os.path.split(ftp_file_path)[-1])
+                        os.path.split(ftp_path)[-1])
         return localfn 
 
-    def connect(self):
+    def upload(self, local_path, ftp_path):
+        f = open(local_path, 'r')
+        
+        self.sendcmd("TYPE I")
+        cout.outs("CornellFTP - Starting upload of: %s" % \
+                    os.path.split(local_path)[-1])
         try:
-            myFtp = M2Crypto.ftpslib.FTP_TLS()
-            myFtp.connect(self.host, self.port)
-            myFtp.auth_tls()
-            myFtp.set_pasv(1)
-            cout.outs("CornellFTP - Connected.")
-        except Exception,e:
-            raise CornellFTPConnectionError( "%s" % str(e) )
-        return myFtp
-
-    def login(self,connection):
-        try:
-            connection.login(self.username, self.password)
-            cout.outs("CornellFTP - Logged in.")
-            return connection
+            self.storbinary("STOR "+ftp_path, f)
         except Exception, e:
-            raise CornellFTPLoginError( "%s" % str(e) )
+            cout.outs("CornellFTP - Upload of %s failed: %s" % \
+                        (os.path.split(local_path)[-1], str(e)))
+            raise CornellFTPError("Could not store binary file (%s) " 
+                                    "on FTP server: %s" % (ftp_path, str(e)))
+        else:
+            cout.outs("CornellFTP - Finished upload of: %s" % \
+                        os.path.split(local_path)[-1])
+        finally:
+            print "closing file"
+            f.close()
+        
+        # Check the size of the uploaded file
+        ftp_size = self.size(ftp_path)
+        local_size = os.path.getsize(local_path)
+        if ftp_size == local_size:
+            cout.outs("CornellFTP - Upload of %s successful." % \
+                    os.path.split(local_path)[-1])
+        else:
+            cout.outs("CornellFTP - Upload of %s failed! " \
+                        "File sizes of local file and " \
+                        "uploaded file on FTP server " \
+                        "don't match (%d != %d)." % \
+                        (os.path.split(local_path)[-1], local_size, ftp_size))
+            raise CornellFTPError("File sizes of local file and " \
+                                    "uploaded file on FTP server " \
+                                    "don't match (%d != %d)." % \
+                                    (local_size, ftp_size))
 
-    def list_files(self,login_connection,ftp_path):
-        if not login_connection:
-            raise Exception('CornellFTP - Not Connected')
-        return login_connection.nlst(ftp_path)
 
-    def write(self, block):
-        self.downloading_file.write(block)
-        self.downloading_file.flush()
-        os.fsync(self.downloading_file)
-
-
-class CornellFTPConnectionError(Exception):
-    def __init__(self, value):
-        self.parameter = value
-    def __str__(self):
-        return repr(self.parameter)
-
-class CornellFTPLoginError(Exception):
-    def __init__(self, value):
-        self.parameter = value
-    def __str__(self):
-        return repr(self.parameter)
+class CornellFTPError(Exception):
+    pass
