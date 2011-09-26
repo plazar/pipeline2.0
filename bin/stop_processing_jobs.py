7ef3ebb9569c6b6f2f06bda62074cdd75d3b424e
Patrick Lazarus <plazar@physics.mcgill.ca>
2011-09-26 23:05:09 +0200

    Blind attempt at adding stop-processed-jobs-functionality to 'stop_processing_jobs.py'.
    Now jobs marked as "processed", waiting to be uploaded, should be able to be stopped/failed.

--- bin/stop_processing_jobs.py
+++ bin/stop_processing_jobs.py
@@ -12,48 +12,69 @@ This script allows safe/force-fail removal of the job from the Pipeline
 """
 
 def main():
-    for queue_id in args:
-        job_submits = jobtracker.query("SELECT id, job_id, queue_id " \
-                                       "FROM job_submits " \
-                                       "WHERE queue_id LIKE '%s'" % queue_id)
-        if len(job_submits) != 1:
+    jobsubmit_ids = options.submit_ids
+    queue_ids = args + options.queue_ids
+    for queue_id in queue_ids:
+        qids = jobtracker.query("SELECT id " \
+                                "FROM job_submits " \
+                                "WHERE queue_id LIKE '%s'" % queue_id)
+        if len(qids) != 1:
             sys.stderr.write("Bad number (%d) of job submissions for queue " \
-                                "ID provided: %s\n" % (len(job_submits), queue_id))
+                                "ID provided: %s\nSkipping...\n" % (len(qids), queue_id))
+        else:
+            jobsubmit_ids.append(qids[0]['id'])
+        
+    for jobsubmit_id in jobsubmit_ids:
+        job_submits = jobtracker.query("SELECT id, job_id, status, queue_id " \
+                                       "FROM job_submits " \
+                                       "WHERE id LIKE '%s'" % jobsubmit_id)
+        if len(job_submits) != 1:
+            sys.stderr.write("Bad number (%d) of job submissions for job submit " \
+                                "ID provided: %s\nSkipping...\n" % (len(job_submits), jobsubmit_id))
+            continue
         elif config.jobpooler.queue_manager.is_running(job_submits[0]['queue_id']):
-            print "Stopping job: %s" % job_submits[0]['queue_id']
+            isrunning = True
+        elif job_submits[0]['status'] == 'processed' and options.also_processed:
+            isrunning = False
+        else:
+            sys.stderr.write("The job submit ID/queue ID provided is invalid. " \
+                             "This code only allows jobs currently running " \
+                             "(i.e. in the queue), or job submits with " \
+                             "status='processed' (if the --also-processed flag " \
+                             "is given), to be stopped/failed. Sorry. Please " \
+                             "try again!\nSkipping...\n")
+            continue
 
-            queries = []
-            if options.fail:
-                queries.append("UPDATE job_submits " \
-                               "SET status='stopped', " \
-                                    "updated_at='%s', " \
-                                    "details='Job was manually failed' " \
-                               "WHERE id=%d" % \
-                                (jobtracker.nowstr(), job_submits[0]['id']))
-                queries.append("UPDATE jobs " \
-                               "SET status='failed', " \
-                                    "updated_at='%s', " \
-                                    "details='Job was manually failed' " \
-                               "WHERE id=%d" % \
-                                (jobtracker.nowstr(), job_submits[0]['job_id']))
-            else:
-                queries.append("DELETE FROM job_submits " \
-                               "WHERE id=%d" % job_submits[0]['id'])
-                queries.append("UPDATE jobs " \
-                               "SET status='retrying', " \
-                                    "updated_at='%s', " \
-                                    "details='Job was manually removed, politely' " \
-                               "WHERE id=%d" % \
-                                (jobtracker.nowstr(), job_submits[0]['job_id']))
-            jobtracker.query(queries)
+        queries = []
+        if options.fail:
+            queries.append("UPDATE job_submits " \
+                           "SET status='stopped', " \
+                                "updated_at='%s', " \
+                                "details='Job was manually failed' " \
+                           "WHERE id=%d" % \
+                            (jobtracker.nowstr(), job_submits[0]['id']))
+            queries.append("UPDATE jobs " \
+                           "SET status='failed', " \
+                                "updated_at='%s', " \
+                                "details='Job was manually failed' " \
+                           "WHERE id=%d" % \
+                            (jobtracker.nowstr(), job_submits[0]['job_id']))
+        else:
+            queries.append("DELETE FROM job_submits " \
+                           "WHERE id=%d" % job_submits[0]['id'])
+            queries.append("UPDATE jobs " \
+                           "SET status='retrying', " \
+                                "updated_at='%s', " \
+                                "details='Job was manually removed, politely' " \
+                           "WHERE id=%d" % \
+                            (jobtracker.nowstr(), job_submits[0]['job_id']))
+        jobtracker.query(queries)
+        if isrunning:
+            print "Stopping job: %s" % job_submits[0]['queue_id']
             try:
                 config.jobpooler.queue_manager.delete(job_submits[0]['queue_id'])
             except pipeline_utils.PipelineError, e:
                 print "PipelineError: %s" % str(e)
-
-        else:
-            sys.stderr.write("There is no job currently in the queue with " \
-                                "the ID provided: %s\n" % job_submits[0]['queue_id'])
 
 
 if __name__ == "__main__":
@@ -67,11 +88,24 @@ if __name__ == "__main__":
                                         "possibilities are done safely, with respect " \
                                         "to the job-tracker DB. The default is to " \
                                         "remove the jobs (not fail).")
+    parser.add_option('-q', '--queue-id', dest='queue_ids', action='append', \
+                        help="A queue_id of a job to stop. Many -q/--queue-id " \
+                             "options can be provided.", \
+                        default=[])
+    parser.add_option('-s', '--submit-id', dest='submit_ids', action='append', \
+                        help="A jobsubmit_id of a job to stop. Many -s/--submit-id " \
+                             "options can be provided.", \
+                        default=[])
     parser.add_option('-f', '--fail', dest='fail', action='store_true', \
                         help="Remove jobs from the queue and mark them " \
                              "as 'failed' in the job-tracker database. " \
                              "(Default: Remove jobs and don't mark them " \
                              "as 'failed').", \
+                        default=False)
+    parser.add_option('--also-processed', dest='also_processed', action='store_true', \
+                        help="Permit job submits with status 'processed' to be " \
+                             "stopped/failed. (Default: don't stop/fail 'processed' " \
+                             "job submits).", \
                         default=False)
     options, args = parser.parse_args()
     main()
