ff6ba098d3d92f81fa762176f760001d7bb99dac
Patrick Lazarus <plazarus@mpifr-bonn.mpg.de>
2011-09-29 12:13:55 +0200

    Modified 'config/upload' to check return code when getting git hashes.
    Untested.

--- lib/python/config/upload.py
+++ lib/python/config/upload.py
@@ -1,5 +1,6 @@
-import subprocess
+import stringio
 import os
+import pipeline_utils
 import config.basic
 ################################################################
 # Result Uploader Configuration
@@ -7,15 +8,24 @@ import config.basic
 def version_num():
     """Compute version number and return it as a string.
     """
-    prestohash = subprocess.Popen("git rev-parse HEAD", \
-                            cwd=os.getenv('PRESTO'), \
-                            stdout=subprocess.PIPE, shell=True).stdout.read().strip()
-    pipelinehash = subprocess.Popen("git rev-parse HEAD", \
-                            cwd=config.basic.pipelinedir, \
-                            stdout=subprocess.PIPE, shell=True).stdout.read().strip()
-    psrfits_utilshash = subprocess.Popen("git rev-parse HEAD", \
-                            cwd=config.basic.psrfits_utilsdir, \
-                            stdout=subprocess.PIPE, shell=True).stdout.read().strip()
+    strio = StringIO.StringIO()
+    pipeline_utils.execute("git rev-parse HEAD", \
+                            dir=os.getenv('PRESTO'), \
+                            stdout=strio, shell=True)
+    prestohash = strio.getvalue().strip()
+    strio.close()
+    strio = StringIO.StringIO()
+    pipeline_utils.execute("git rev-parse HEAD", \
+                            dir=config.basic.pipelinedir, \
+                            stdout=strio, shell=True)
+    pipelinehash = strio.getvalue().strip()
+    strio.close()
+    strio = StringIO.StringIO()
+    pipeline_utils.execute("git rev-parse HEAD", \
+                            dir=config.basic.psrfits_utilsdir, \
+                            stdout=strio, shell=True)
+    psrfits_utilshash = strio.getvalue().strip()
+    strio.close()
     vernum = 'PRESTO:%s;PIPELINE:%s;PSRFITS_UTILS:%s' % \
                             (prestohash, pipelinehash, psrfits_utilshash)
     return vernum
