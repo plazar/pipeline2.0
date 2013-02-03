39e6bd4629d55ee5e65e195260033c54af8bdb72
Paul Scholz <pscholz@physics.mcgill.ca>
2013-02-03 10:43:22 -0500

    Changed singlepulse and pfd FTP directories to PRESTO3 specific dirs in config/upload.py

--- lib/python/config/upload.py
+++ lib/python/config/upload.py
@@ -19,8 +19,8 @@ def version_num():
     return vernum
 
 # Directory on the FTP server to upload PFDs and singlepulse files (do not change unless asked by Adam)
-pfd_ftp_dir = 'pfd'
-sp_ftp_dir = 'singlePulse/reprocessing'
+pfd_ftp_dir = 'pfd/PRESTO3'
+sp_ftp_dir = 'singlePulse/PRESTO3'
 
 import upload_check
 upload_check.upload.populate_configs(locals())
